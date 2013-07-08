03a50161d81a221b8c755f399e1076e419596137
Cristina Benito <cristina.benito@4thsource.com>
2013-07-08 11:00:10 -0500

    Primer aplicacion
    Creacion del primer repositorio 8 julio 2013

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '0a80dbc1427e98eb4697463db25cfeb7e1bd8f83690a605633f7590308eae1210a38503d5ffe89e701e4f6f87cf583e62c1a888e831fbcf5078aae6c0ee146af'
